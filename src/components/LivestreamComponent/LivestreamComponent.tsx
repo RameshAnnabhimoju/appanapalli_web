a85d628efe8bddb8d199fc3665beb16e49e5e1d6
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2025-06-06 15:56:01 +0530

    changed yt link

--- src/components/LivestreamComponent/LivestreamComponent.tsx
+++ src/components/LivestreamComponent/LivestreamComponent.tsx
@@ -17,7 +17,7 @@ const LivestreamComponent = () => {
         </div>
       </div>
       <iframe
-        src="https://www.youtube.com/watch?v=l1W4aUmaAL4?autoplay=1"
+        src="https://www.youtube.com/watch?v=l1W4aUmaAL4"
         allow="autoplay; encrypted-media"
         allowFullScreen
         style={{
