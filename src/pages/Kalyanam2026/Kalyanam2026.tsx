327aac04e5bb2c5d58ce7e81685d7856eb7d7176
SuryaYalangi <suryay@nirvahak.net>
2026-06-24 23:49:46 +0530

    Missing image-5

--- src/pages/Kalyanam2026/Kalyanam2026.tsx
+++ src/pages/Kalyanam2026/Kalyanam2026.tsx
@@ -3,9 +3,10 @@ import "./Kalyanam2026.css";
 import img2 from "../../assets/images/kalyanam-2.jpeg";
 import img3 from "../../assets/images/kalyanam-3.jpeg";
 import img4 from "../../assets/images/kalyanam-4.jpeg";
+import img5 from "../../assets/images/kalyanam-5.jpeg";
 
 const Kalyanam2026 = () => {
-  const images = [img2, img3, img4];
+  const images = [img2, img3, img4, img5];
 
   return (
     <div className="kalyanam-container">
