91f2c8769fb4f8ce2ac840bd959fd8e7dc23134e
SuryaYalangi <suryay@nirvahak.net>
2026-06-24 23:21:29 +0530

    minior changes

--- src/components/CarouselComponent/CarouselComponent.tsx
+++ src/components/CarouselComponent/CarouselComponent.tsx
@@ -1,10 +1,10 @@
-import KalyanamBanner from "../KalyanamBanner/KalyanamBanner";
+// import KalyanamBanner from "../KalyanamBanner/KalyanamBanner";
 import "./CarouselComponent.css";
 import kalyanamPhoto from "../../assets/images/kalyanam 1.jpeg";
 import { useNavigate } from "react-router-dom";
-import { Carousel } from "react-bootstrap";
-import godhaReel1 from "../../assets/videos/GODHA-REEL-1.mp4";
-import godhaReel2 from "../../assets/videos/GODHA-REEL-2.mp4";
+// import { Carousel } from "react-bootstrap";
+// import godhaReel1 from "../../assets/videos/GODHA-REEL-1.mp4";
+// import godhaReel2 from "../../assets/videos/GODHA-REEL-2.mp4";
 
 const CarouselComponent = () => {
   const navigate = useNavigate();
